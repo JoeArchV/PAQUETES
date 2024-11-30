b666d8ad453e582eabcac6f7ea59aeaf2c2c0076
JoeArch <168229988+JoeArchV@users.noreply.github.com>
2024-11-29 22:54:43 -0400

    Add files via upload

---
+++ instalador.py
@@ -0,0 +1,150 @@
+import os
+
+os.system ('sudo pacman -Syyu')
+
+os.system ('clear')
+
+os.system ('sudo pacman -S --needed git base-devel')
+
+os.system ('clear')
+
+os.system ('sudo pacman -S discover')
+
+os.system ('clear')
+
+os.system ('sudo pacman -S flatpak')
+
+os.system ('clear')
+
+os.system ('git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si')
+
+os.system ('clear')
+
+os.system ('sudo pacman -S spotify-launcher')
+
+os.system ('clear')
+
+os.system ('sudo pacman -S discord')
+
+os.system ('clear')
+
+os.system ('yay -S cava-git')
+
+os.system ('clear')
+
+os.system ('sudo pacman -S adb')
+
+os.system ('clear')
+
+os.system ('sudo pacman -S wine')
+
+os.system ('clear')
+
+os.system ('sudo pacman -S wine-mono')
+
+os.system ('clear')
+
+os.system ('sudo pacman -S aircrack-ng wifite')
+
+os.system ('clear')
+
+os.system ('sudo pacman -S neofetch')
+
+os.system ('clear')
+
+os.system ('sudo pacman -S reaver')
+
+os.system ('clear')
+
+os.system ('sudo pacman -S bully')
+
+os.system ('clear')
+
+os.system ('sudo pacman -S wireshark-cli')
+
+os.system ('clear')
+
+os.system ('sudo pacman -S cowpatty')
+
+os.system ('clear')
+
+os.system ('sudo pacman -S pyrit')
+#repositorios blackarch
+os.system ('clear')
+
+os.system ('sudo pacman -S hcxdumptool')
+
+os.system ('clear')
+
+os.system ('yay -S google-chrome')
+
+os.system ('clear')
+
+os.system ('sudo pacman -Rns firefox')
+
+os.system ('clear')
+
+os.system ('sudo pacman -S scrcpy')
+
+os.system ('clear')
+
+os.system ('sudo pacman -S nwg-displays')
+
+os.system ('clear')
+
+os.system ('sudo pacman -S steam')
+
+os.system ('clear')
+
+os.system ('sudo pacman -S nmap')
+
+os.system ('clear')
+
+os.system ('sudo pacman -S ruby')
+
+os.system ('clear')
+
+os.system ('sudo pacman -S metasploit')
+
+os.system ('clear')
+
+os.system ('sudo pacman -S fluxion')
+
+os.system ('clear')
+
+os.system ('sudo pacman -S bluez blueman pulseaudio-bluetooth')
+
+os.system ('clear')
+
+os.system ('sudo pacman -S vlc')
+
+os.system ('clear')
+
+os.system ('sudo pacman -S sqlmap')
+
+os.system ('clear')
+
+os.system ('sudo pacman -S hydra john')
+
+os.system ('clear')
+
+os.system ('sudo pacman -S nikto wpscan legion curl')
+
+os.system ('clear')
+
+os.system ('yay -S visual-studio-code-bin')
+
+os.system ('clear')
+
+os.system ('flatpak install flathub org.shotcut.Shotcut')
+
+os.system ('clear')
+
+os.system ('sudo pacman -S obs-studio')
+
+os.system ('clear')
+
+os.system ('sudo pacman -S ranger')
+
+os.system ('clear')
+print ('TODO INSTALADO')
